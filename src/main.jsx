496eef3f3039e40428d5dd9dccd266fcac78cd26
H-Kahil <hasan.kahil@outlook.com>
2025-04-09 13:56:42 +0000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+// Import the dev tools and initialize them
+import { TempoDevtools } from "tempo-devtools";
+TempoDevtools.init();
+
+createRoot(document.getElementById("root")).render(<App />);
